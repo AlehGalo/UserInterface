1dd6f187409332c555a474d03c711780a0bbd9bc
Aleh <ferranostra@gmail.com>
2017-11-01 11:55:21 +0300

    Table added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from './Grid.js';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
             Hello ReactJS start APP mobx as state holder
         </p>
+          <Grid/>
       </div>
     );
   }
---
+++ src/Grid.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Grid extends Component {
+    getInitialState(){
+        return [];
+    },
+    render() {
+        return (
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default Grid;
