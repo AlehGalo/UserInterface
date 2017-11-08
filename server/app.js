1c01351972ed2dcdfc5ebefe9564dba93a8c7180
Aleh <ferranostra@gmail.com>
2017-11-08 03:39:49 +0300

    Add mongoose (MongoDB driver)

--- server/app.js
+++ server/app.js
@@ -6,29 +6,56 @@ const path = require('path');
 
 const app = express();
 
+const mongoose = require ("mongoose"); // The reason for this demo.
+const uriString = process.env.MONGODB_URI;
+
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uriString, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uriString + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uriString);
+    }
+});
+
+const userSchema = mongoose.Schema({
+    firstName: string,
+    lastName: string,
+    userName: string
+});
+
+
 // Answer API requests.
 app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
-    res.send('{"message":' + process.env.PORT || 9000 + '}');
+    res.send('{"message":"' + (process.env.PORT || 9000) + '"}');
 });
 
 app.get('/grid', function(request, response){
-    let numberOfItems = 5000;
-    let array = [];
+    let userModel = mongoose.model('userModel', userSchema);
 
-    for (let i = 0; i < numberOfItems; i++) {
-        array[i] = [];
-        for (let j = 0; j < 4; j++) {
-            array[i][j] = i + "+=+" + j;
-        }
-    }
-    response.send(JSON.stringify(array));
+    userModel.find(function(err, users){
+        if (err) return handleError(err);
+        response.send(JSON.stringify(users));
+    });
+    //
+    // let numberOfItems = 5000;
+    // let array = [];
+    //
+    // for (let i = 0; i < numberOfItems; i++) {
+    //     array[i] = [];
+    //     for (let j = 0; j < 4; j++) {
+    //         array[i][j] = i + "+=+" + j;
+    //     }
+    // }
+    // response.send(JSON.stringify(array));
 });
 
 
